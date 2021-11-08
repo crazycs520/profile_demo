9fa46730dbf36dc2cd72579bfede4dfabe48f6b5
Zhenchi <zhongzc_arch@outlook.com>
2021-11-08 11:39:16 +0800

    refine
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- demo4/src/main.rs
+++ demo4/src/main.rs
@@ -1,29 +1,29 @@
-use std::sync::Arc;
+#![feature(bench_black_box)]
+
+use std::collections::HashMap;
+use std::hint::black_box;
 use std::os::raw::c_int;
 use std::ptr::null_mut;
-use std::sync::atomic::{AtomicI64,Ordering};
-use nix::sys::signal;
-use std::{thread, time};
 use std::thread::ThreadId;
-use std::time::{SystemTime};
-use std::collections::HashMap;
-use parking_lot::RwLock;
+use std::{thread, time};
 
+use nix::sys::signal;
+use parking_lot::Mutex;
+use lazy_static::lazy_static;
 
-lazy_static::lazy_static! {
-    pub(crate) static ref PROFILER: RwLock<HashMap<ThreadId,i64>> = RwLock::new(HashMap::new());
-    pub static ref INT_COUNTER: Arc<AtomicI64> = Arc::new(AtomicI64::new(0));
+lazy_static! {
+    static ref PROFILER: Mutex<HashMap<ThreadId, i64>> = Mutex::new(HashMap::new());
 }
 
 #[repr(C)]
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Timeval {
     pub tv_sec: i64,
     pub tv_usec: i64,
 }
 
 #[repr(C)]
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Itimerval {
     pub it_interval: Timeval,
     pub it_value: Timeval,
@@ -36,74 +36,70 @@ extern "C" {
 const ITIMER_PROF: c_int = 2;
 
 fn main() {
-    let freq = 100;
-    let interval = 1e6 as i64 / i64::from(freq);
-        let it_interval = Timeval {
-            tv_sec: interval / 1e6 as i64,
-            tv_usec: interval % 1e6 as i64,
-        };
-    let it_value = it_interval.clone();
+    let freq = 99;
+    let interval = 1_000_000 / freq;
+    let timeval = Timeval {
+        tv_sec: interval / 1_000_000,
+        tv_usec: interval % 1_000_000,
+    };
     unsafe {
-            setitimer(
-                ITIMER_PROF,
-                &mut Itimerval {
-                    it_interval,
-                    it_value,
-                },
-                null_mut(),
-            )
-        };
+        setitimer(
+            ITIMER_PROF,
+            &mut Itimerval {
+                it_interval: timeval,
+                it_value: timeval,
+            },
+            null_mut(),
+        )
+    };
 
     let handler = signal::SigHandler::Handler(perf_signal_handler);
-    unsafe { signal::signal(signal::SIGPROF, handler) }; 
-    println!("Hello, world!");
-    
-    for n in 0..2 {
+    unsafe { signal::signal(signal::SIGPROF, handler).expect("setup profile handler failed") };
+
+    for _thread in 0..2 {
         thread::spawn(move || {
             println!("thread {:?} started", thread::current().id());
-            while true {
-                let mut i =0;
+
+            // heavy cpu workload
+            loop {
+                let mut i = 0;
                 while i < 100000000 {
-                    i = i + 1;
+                    i += 1;
                 }
+                black_box(i);
             }
         });
     }
 
-    for n in 0..2 {
+    for _thread in 0..2 {
         thread::spawn(move || {
             println!("thread {:?} started", thread::current().id());
-            while true {
+            loop {
                 let duration = time::Duration::from_millis(1);
                 thread::sleep(duration);
             }
         });
     }
 
-    while true {
-        // let mut i =0;
-        // while i < 100000000 {
-        //     i = i + 1;
-        // }
+    loop {
         let duration = time::Duration::from_millis(1000);
         thread::sleep(duration);
-        println!("");
-        if let Some(guard) = PROFILER.try_write() {
-            for (id,cnt) in guard.iter(){
+
+        println!("-------------------");
+        if let Some(guard) = PROFILER.try_lock() {
+            for (id, cnt) in guard.iter() {
                 println!("id {:?}, {:?}", id, cnt);
             }
         }
     }
 }
 
-
 #[no_mangle]
 #[allow(clippy::uninit_assumed_init)]
 extern "C" fn perf_signal_handler(_signal: c_int) {
-    INT_COUNTER.fetch_add(1, Ordering::SeqCst);
     let thread_id = thread::current().id();
-    if let Some(mut guard) = PROFILER.try_write() {
+    if let Some(mut guard) = PROFILER.try_lock() {
         let cnt = guard.entry(thread_id).or_insert(0);
-        *cnt = *cnt + 1;
+        *cnt += 1;
     }
 }
--- demo5/src/main.rs
+++ demo5/src/main.rs
@@ -1,46 +1,44 @@
-use nix::sys::signal;
-use parking_lot::RwLock;
+#![feature(bench_black_box)]
+
+use std::cell::Cell;
 use std::collections::HashMap;
+use std::hint::black_box;
+use std::mem;
 use std::os::raw::c_int;
 use std::ptr::null_mut;
-use std::sync::atomic::{AtomicI64, Ordering};
 use std::{thread, time};
 
-lazy_static::lazy_static! {
-    pub(crate) static ref PROFILER: RwLock<HashMap<i64,i64>> = RwLock::new(HashMap::new());
+use lazy_static::lazy_static;
+use nix::sys::signal;
+use parking_lot::Mutex;
+
+lazy_static! {
+    static ref PROFILER: Mutex<HashMap<i64, i64>> = Mutex::new(HashMap::new());
 }
 
 thread_local! {
-    pub static REQUEST_TAG: AtomicI64 = AtomicI64::new(0);
+    static REQUEST_TAG: Cell<i64> = Cell::new(0);
 }
 
 fn set_thread_tag(id: i64) {
-    REQUEST_TAG.with(|tag| {
-        tag.store(id, Ordering::SeqCst);
-    });
+    REQUEST_TAG.with(|tag| tag.set(id));
 }
 
 fn get_thread_tag() -> i64 {
-    let mut id = 0;
-    REQUEST_TAG.with(|tag| {
-        id = tag.load(Ordering::SeqCst);
-    });
-    return id;
+    REQUEST_TAG.with(|tag| tag.get())
 }
 
 fn main() {
     setup_timer();
 
-    for _n in 0..2 {
-        thread::spawn(move || {
-            loop {
-                for id in 1..=4 {
-                    set_thread_tag(id);
+    for _thread in 0..2 {
+        thread::spawn(move || loop {
+            for request_tag_id in 1..=4 {
+                set_thread_tag(request_tag_id);
 
-                    handle_request(id);
+                handle_request(request_tag_id);
 
-                    set_thread_tag(0);
-                }
+                set_thread_tag(0);
             }
         });
     }
@@ -49,19 +47,15 @@ fn main() {
         let duration = time::Duration::from_millis(1000);
         thread::sleep(duration);
 
-        let mut summary = HashMap::new();
-        if let Some(mut guard) = PROFILER.try_write() {
-            for (id, cnt) in guard.iter() {
-                summary.insert(*id, *cnt);
-            }
-            *guard = HashMap::new();
+        let summary = PROFILER.try_lock().map(|mut guard| mem::take(&mut *guard));
+        if summary.is_none() {
+            continue;
         }
+        let summary = summary.unwrap();
 
-        println!("\n\nrequest cpu usage:");
-        let mut total = 0;
-        for (_id, cnt) in summary.iter() {
-            total = total + cnt;
-        }
+        println!("---------------------------------------");
+        println!("request cpu usage:");
+        let total = summary.values().sum::<i64>();
         for (id, cnt) in summary.iter() {
             println!(
                 "request_id {:?}, count: {:?}, cpu usage: {}%",
@@ -74,28 +68,24 @@ fn main() {
 }
 
 const CYCLE: i64 = 1000000;
-
 fn handle_request(id: i64) {
-    // if id == 4 {
-    //     thread::sleep(time::Duration::from_millis(1));
-    //     return
-    // }
     let n = id * CYCLE;
     let mut sum = 0;
     for i in 0..n {
-        sum = sum + i * 2;
+        sum += i * 2;
     }
+    black_box(sum);
 }
 
 #[repr(C)]
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Timeval {
     pub tv_sec: i64,
     pub tv_usec: i64,
 }
 
 #[repr(C)]
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Itimerval {
     pub it_interval: Timeval,
     pub it_value: Timeval,
@@ -109,33 +99,32 @@ const ITIMER_PROF: c_int = 2;
 
 fn setup_timer() {
     let freq = 99;
-    let interval = 1e6 as i64 / i64::from(freq);
-    let it_interval = Timeval {
-        tv_sec: interval / 1e6 as i64,
-        tv_usec: interval % 1e6 as i64,
+    let interval = 1_000_000 / freq;
+    let timeval = Timeval {
+        tv_sec: interval / 1_000_000,
+        tv_usec: interval % 1_000_000,
     };
-    let it_value = it_interval.clone();
     unsafe {
         setitimer(
             ITIMER_PROF,
             &mut Itimerval {
-                it_interval,
-                it_value,
+                it_interval: timeval,
+                it_value: timeval,
             },
             null_mut(),
         )
     };
 
     let handler = signal::SigHandler::Handler(perf_signal_handler);
-    unsafe { signal::signal(signal::SIGPROF, handler) };
+    unsafe { signal::signal(signal::SIGPROF, handler).expect("setup profile handler failed") };
 }
 
 #[no_mangle]
 #[allow(clippy::uninit_assumed_init)]
 extern "C" fn perf_signal_handler(_signal: c_int) {
     let tag = get_thread_tag();
-    if let Some(mut guard) = PROFILER.try_write() {
+    if let Some(mut guard) = PROFILER.try_lock() {
         let cnt = guard.entry(tag).or_insert(0);
-        *cnt = *cnt + 1;
+        *cnt += 1;
     }
 }
